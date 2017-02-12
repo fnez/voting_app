7f984cbdbe9e1c209293c37ba6e1e21be82a1c5f
Jordan Smith <cjordansmith@gmail.com>
2017-02-12 03:46:28 -0600

    change app to dynamic with ProductList component and Product child components

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,65 @@
+//  ./public/js/app.js
 
+//  ProductList component
+class ProductList extends React.Component {
+
+    render() {
+        const products = Seed.products.sort((a, b) => (
+            b.votes - a.votes
+        ));
+
+        const productComponents = products.map((product) => (
+            <Product
+                key={'product-' + product.id}
+                id={product.id}
+                title={product.title}
+                description={product.description}
+                url={product.url}
+                votes={product.votes}
+                submitterAvatarUrl={product.submitterAvatarUrl}
+                productImageUrl={product.productImageUrl}
+            />
+        ));
+
+        return (
+            <div className='ui unstackable items'>
+                {productComponents}
+            </div>
+        );
+    }
+}
+
+//  Product component
+//  child component of ProductList
+class Product extends React.Component {
+
+    render() {
+        return(
+            <div className='item'>
+                <div className='image'>
+                    <img src={this.props.productImageUrl} />
+                </div>
+                <div className='middle aligned content'>
+                    <div className='header'>
+                        <a><i className='large caret up icon' /></a>{this.props.votes}
+                    </div>
+                    <div className='description'>
+                        <a href={this.props.url}>{this.props.title}</a>
+                        <p>{this.props.description}</p>
+                    </div>
+                    <div className='extra'>
+                        <span>Submitted by:</span>
+                        <img className='ui avatar image' src={this.props.submitterAvatarUrl} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+//  instruct React to render this PRODUCTLIST inside a specific DOM node
+//  must be last element in document ( for now )
+ReactDOM.render(
+    <ProductList />,
+    document.getElementById('content')
+);
